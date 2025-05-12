b8d2a7025188b8418e15de668b95203483b7ca5a
Calvin Chen <kmcxr21@126.com>
2025-05-12 18:31:24 +0930

    updateing

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,31 @@
+# from flask import Flask
+
+# def create_app():
+#     app = Flask(__name__)
+#     app.config.from_object('config.Config')
+
+#     from . import routes
+#     app.register_blueprint(routes.bp)
+
+#     return app
+
 from flask import Flask
+from backend.app.auth.routes import bp as auth_bp
+from backend.app.item import bp as item_bp
+
+from backend.app.db.database import engine, close_db
+from backend.app.auth.models import Base
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
+    
+    # Config
+    app.config.from_mapping(SECRET_KEY="dev")
 
-    from . import routes
-    app.register_blueprint(routes.bp)
+    # Routes
+    app.register_blueprint(auth_bp)
+    app.teardown_appcontext(close_db)
 
     return app
+
+app = create_app()
